9e4da70ce6d13af93963a8df3f75fcb5589d3ba0
WaterMan-Joy <nietzsche21st@gmail.com>
2022-10-27 17:54:16 +0900

    editing2

--- models/user.js
+++ models/user.js
@@ -11,4 +11,10 @@ const userShcema = new mongoose.Schema ({
     }
 })
 
+userShcema.static.findAndValidate = async function (username, password) {
+    const foundUser = await this.findOne({ username: username });
+    const isValid = await bcrypt.compare(password, foundUser.password);
+    return isValid ? foundUser : false;
+}
+
 module.exports = mongoose.model('User', userShcema);
