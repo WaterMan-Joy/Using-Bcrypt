3b3c3c2a870bc8d47524f462a49de4f06d9acc5d
WaterMan-Joy <nietzsche21st@gmail.com>
2022-10-27 15:14:41 +0900

    add register and login

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const User = require('./models/user');
 const bcrypt = require('bcrypt');
+const session = require('express-session');
 
 const app = express();
 
@@ -17,11 +18,18 @@ app.set('view engine', 'ejs');
 app.set('views', 'views');
 
 app.use(express.urlencoded({ extended: true }));
+app.use(session({
+    secret: 'notagoodsecret',
+    resave: false,
+    saveUninitialized: true,
+
+}))
 
 app.get('/', (req, res) => {
     res.send('Hi')
 })
 
+// TODO: POST REGISTER
 app.post('/register', async (req, res) => {
     const { username, password } = req.body;
     const hash = await bcrypt.hash(password, 12);
@@ -30,15 +38,18 @@ app.post('/register', async (req, res) => {
         password: hash,
     })
     await user.save();
-    res.redirect('/');
+    req.session.user_id = user._id;
+    res.redirect('/login');
 })
 
+// TODO: POST LOGIN
 app.post('/login', async (req, res) => {
     const { username, password } = req.body;
     const user = await User.findOne({ username: username });
     const validPassword = await bcrypt.compare(password, user.password);
     if (validPassword) {
-        res.send('Login success');
+        req.session.user_id = user._id;
+        res.redirect('/secret');
     }
     else {
         res.redirect('/login');
@@ -46,18 +57,27 @@ app.post('/login', async (req, res) => {
 
 })
 
+// TODO: GET LOGIN
 app.get('/login', (req, res) => {
     res.render('login');
 })
 
+// TODO: GET REGISTER
 app.get('/register', (req, res) => {
     res.render('register')
 })
 
+// TODO: GET SECRET
 app.get('/secret', (req, res) => {
-    res.send('This is secret! you can not see me');
+    if (req.session.user_id) {
+        res.send('It is saved!')
+    }
+    else {
+        res.send('It is Not saved!!');
+    }
 })
 
+// TODO: LISTEN
 app.listen(3000, () => {
     console.log('LITENING ON PORT 3000!!')
 })
